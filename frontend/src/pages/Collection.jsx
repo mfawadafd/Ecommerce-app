f7ddd3092a328ddfe9f5986759e1b49568eafc61
Muhammad Fawad <mfawadafridi83@gmail.com>
2026-06-09 23:33:34 -0700

    Search_function

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from '../components/Title'
 import ProductItem from '../components/ProductItem'
 
 const Collection = () => {
-  const { products } = useContext(ShopContext)
+  const { products, search, showSearch } = useContext(ShopContext)
   const [showFilter, setShowFilter] = useState(false)
   const [filterProducts, setFilterProducts] = useState([])
   const [Category, setCategory] = useState([])
@@ -32,6 +32,10 @@ const Collection = () => {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+    if(showSearch && search){
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    }
+
     if (Category.length > 0) {
       productsCopy = productsCopy.filter(item => Category.includes(item.category))
     }
@@ -59,7 +63,7 @@ const Collection = () => {
 
   useEffect(() => {
     applyFilter()
-  }, [Category, subCategory])
+  }, [Category, subCategory, search, showSearch])
 
   useEffect(() => {
     sortProducts();
