ff56ac1d15db9bb99ea7e8e8da3ff89c4520a669
Muhammad Fawad <mfawadafridi83@gmail.com>
2026-06-15 03:03:06 -0700

    Order&Payment

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import CartTotal from '../components/CartTotal'
 
 const Cart = () => {
-  const { products, currency, cartItems } = useContext(ShopContext)
+  const { products, currency, cartItems, updateQuantity, navigate } = useContext(ShopContext)
 
   const [cartData, setCartData] = useState([]);
 
@@ -39,12 +41,26 @@ const Cart = () => {
                     <img className='w-16 sm:w-20 ' src={productData.image[0]} alt="" />
                     <div>
                       <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
+                      <div className='flex items-center gap-5 mt-2'>
+                          <p>{currency}{productData.price}</p>
+                          <p className='px-2 sm:px-3 sm:py-1 border bg-slat'>{item.size}</p>
+                      </div>
                     </div>
                 </div>
+                <input onChange={(e)=> e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id, item.size, Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
+                <img  onClick={()=>updateQuantity(item._id, item.size, 0)} className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="" />
               </div>
             )
           })
         }
+      </div>
+      <div className='flex justify-end my-20'>
+          <div className='w-full sm:w-[450px]'>
+          <CartTotal/>
+          <div className='w-full text-end'>
+            <button onClick={()=>navigate('/PlaceOrder')} className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO CHECKOUT</button>
+          </div>
+          </div>
       </div>
     </div>
   )
--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title';
 
 const Orders = () => {
+
+  const {currency, products} = useContext(ShopContext);
+
   return (
-    <div>Orders</div>
+    <div className='border-t pt-16'>
+      <div className='text-2xl'>
+        <Title text1={'MY'} text2={'ORDERS'}/>
+      </div>
+      <div className=''>
+        {
+          products.slice(1,4).map((item,index)=>(
+            <div key={index} className='py-4 border-t border-b text-gary-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
+              <div className='flex items-stat gap-6 text-sm'>
+                  <img className='w-16 sm:w-20' src={item.image[0]} alt="" />
+                  <div>
+                    <p className='sm:text-base font-medium'>{item.name}</p>
+                    <div className='flex items-center gap-3 mt-2 text-base text-gary-700'>
+                        <p className='text-lg'>{currency}{item.price}</p>
+                        <p>Quantity: 1</p>
+                        <p>Size: M</p>
+                    </div>
+                    <p className='mt-2'>Date: <span className="text-gary-400">15,jun,2026</span></p>
+                  </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
